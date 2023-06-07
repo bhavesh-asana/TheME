0054796970ef305373d927ec6c17d484dc7607b4
bhavesh-asana <asanabhavesh@gmail.com>
2023-06-06 22:46:20 -0500

    Initialized Server Framework

---
+++ MyServices/src/main/java/com/bhavesh/myservices/MyServicesApplication.java
@@ -0,0 +1,13 @@
+package com.bhavesh.myservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyServicesApplication.class, args);
+    }
+
+}
---
+++ MyServices/src/test/java/com/bhavesh/myservices/MyServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhavesh.myservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
