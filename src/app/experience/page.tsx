d7eba8794c302f62b04eae34e66e2d51efa96baa
bhavesh-asana <asanabhavesh@gmail.com>
2024-05-06 10:14:47 -0500

    updated pages

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,40 +1,113 @@
-import React from 'react'
+import React from "react";
 
 export default function experience() {
   return (
-    <main className='space-y-4 m-7 pl-7 ml-10'>
-      <div className='container bg-rose-200/10 rounded-xl p-4 justify-around transform transition duration-500 hover:scale-105'>
-        <h1 className='text-2xl text-rose-600'>Google Summer of Code</h1>
-        <h2 className='text-l text-rose-800'>Software Developer</h2>
-        <ol className='card bg-white/10 rounded-xl px-7 py-3 justify-around text-justify hover:list-disc shadow-xl'>
-          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde repellendus quae ut qui commodi nesciunt, sed quas veniam asperiores recusandae esse necessitatibus animi tempora ratione voluptatem, tenetur fuga error eum!</li>
+    <main className="space-y-4 m-7 pl-7 ml-10">
+      {/* Indiana University */}
+      <div className="container bg-rose-200/10 rounded-xl p-4 justify-around transform transition duration-500 hover:scale-105">
+        <h1 className="text-2xl text-rose-600">Core Researcher</h1>
+        <h2 className="text-l text-rose-800">Indiana University</h2>
+        <ol className="card bg-white/10 rounded-xl px-7 py-3 justify-around text-justify hover:list-disc shadow-xl">
+          <li>
+            Engaged in tasks related to Distributed Systems. Provision and
+            Seamless Management of High-Performance Computing Environment,
+            including integrated compute infrastructure, tooling, and workload
+            schedulers.
+          </li>
+          <li>
+            I worked on the Science and Engineering Application Grid (SEAGrid)
+            scientific gateway, which served 11M XD Service Units and supported
+            almost 25,000 jobs.
+          </li>
+          <li>
+            Responsible for leveraging the performance of deployed scientific
+            applications across a wide range of supercomputers, campus clusters,
+            and cloud computing.
+          </li>
+          <li>
+            Responsible for analyzing the Gaussian‑16 Application with a high
+            volume of data ingestion and fragmenting the output quantities.
+          </li>
+        </ol>
+      </div>
+      {/* Google Summer of Code */}
+      <div className="container bg-rose-200/10 rounded-xl p-4 justify-around transform transition duration-500 hover:scale-105">
+        <h1 className="text-2xl text-rose-600">Software Developer</h1>
+        <h2 className="text-l text-rose-800">Google Summer of Code</h2>
+        <ol className="card bg-white/10 rounded-xl px-7 py-3 justify-around text-justify hover:list-disc shadow-xl">
+          <li>
+            Designed and Implemented a Scientific Gateway Architecture with
+            XSEDE Cyber‑infrastructure for the project Apache Airavata,
+            sponsored by the National Science Foundation (NSF) of America.
+          </li>
+          <li>
+            Built user‑friendly scientific dashboards for visualizing real‑time
+            tabular and graphical data with a two‑factor authentication service
+            in VueJS.
+          </li>
+          <li>
+            Designed and implemented a real‑time data pipeline to process
+            semi‑structured data by integrating thousands of records from
+            different data sources using Apache Kafka.
+          </li>
+          <li>
+            Implemented a Google Remote Procedure Call (gRPC) API paradigm for
+            Bi‑directional message streaming with the use of HTTP/2 for
+            transport.
+          </li>
+          <li>
+            Implemented Protocol Buffers for serializing and de‑serializing the
+            data and communicating in binary.
+          </li>
+          <li>
+            Developed an application in the Django framework, serving as a
+            middleware, to process the data transmission upon achieving the
+            qualified ranking of the data classification algorithms and EDA.
+          </li>
+          <li>
+            Developed a Java Spring Boot application as a backend service to
+            extract data from three different HPC data sources configured in
+            MySQL and MongoDB.
+          </li>
+        </ol>
+      </div>
+      <div className="container bg-rose-200/10 rounded-xl p-4 justify-around transform transition duration-500 hover:scale-105">
+        <h1 className="text-2xl text-rose-600">Data Analyst</h1>
+        <h2 className="text-l text-rose-800">WiseCube</h2>
+        <ol className="card bg-white/10 rounded-xl px-7 py-3 justify-around text-justify hover:list-disc shadow-xl">
+          <li>
+            Worked on the FIXimulator and Banzai tools for the sell‑side and
+            buy‑side in Trading systems.
+          </li>
           <li>dsvads</li>
         </ol>
       </div>
-      <div className='container bg-rose-200/10 rounded-xl p-4 justify-around transform transition duration-500 hover:scale-105'>
-        <h1 className='text-2xl text-rose-600'>Google Summer of Code</h1>
-        <h2 className='text-l text-rose-800'>Software Developer</h2>
-        <ol className='card bg-white/10 rounded-xl px-7 py-3 justify-around text-justify hover:list-disc shadow-xl'>
-          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde repellendus quae ut qui commodi nesciunt, sed quas veniam asperiores recusandae esse necessitatibus animi tempora ratione voluptatem, tenetur fuga error eum!</li>
+      <div className="container bg-rose-200/10 rounded-xl p-4 justify-around transform transition duration-500 hover:scale-105">
+        <h1 className="text-2xl text-rose-600">Google Summer of Code</h1>
+        <h2 className="text-l text-rose-800">Software Developer</h2>
+        <ol className="card bg-white/10 rounded-xl px-7 py-3 justify-around text-justify hover:list-disc shadow-xl">
+          <li>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
+            repellendus quae ut qui commodi nesciunt, sed quas veniam asperiores
+            recusandae esse necessitatibus animi tempora ratione voluptatem,
+            tenetur fuga error eum!
+          </li>
           <li>dsvads</li>
         </ol>
       </div>
-      <div className='container bg-rose-200/10 rounded-xl p-4 justify-around transform transition duration-500 hover:scale-105'>
-        <h1 className='text-2xl text-rose-600'>Google Summer of Code</h1>
-        <h2 className='text-l text-rose-800'>Software Developer</h2>
-        <ol className='card bg-white/10 rounded-xl px-7 py-3 justify-around text-justify hover:list-disc shadow-xl'>
-          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde repellendus quae ut qui commodi nesciunt, sed quas veniam asperiores recusandae esse necessitatibus animi tempora ratione voluptatem, tenetur fuga error eum!</li>
-          <li>dsvads</li>
-        </ol>
-      </div>
-      <div className='container bg-rose-200/10 rounded-xl p-4 justify-around transform transition duration-500 hover:scale-105'>
-        <h1 className='text-2xl text-rose-600'>Google Summer of Code</h1>
-        <h2 className='text-l text-rose-800'>Software Developer</h2>
-        <ol className='card bg-white/10 rounded-xl px-7 py-3 justify-around text-justify hover:list-disc shadow-xl'>
-          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde repellendus quae ut qui commodi nesciunt, sed quas veniam asperiores recusandae esse necessitatibus animi tempora ratione voluptatem, tenetur fuga error eum!</li>
+      <div className="container bg-rose-200/10 rounded-xl p-4 justify-around transform transition duration-500 hover:scale-105">
+        <h1 className="text-2xl text-rose-600">Google Summer of Code</h1>
+        <h2 className="text-l text-rose-800">Software Developer</h2>
+        <ol className="card bg-white/10 rounded-xl px-7 py-3 justify-around text-justify hover:list-disc shadow-xl">
+          <li>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
+            repellendus quae ut qui commodi nesciunt, sed quas veniam asperiores
+            recusandae esse necessitatibus animi tempora ratione voluptatem,
+            tenetur fuga error eum!
+          </li>
           <li>dsvads</li>
         </ol>
       </div>
     </main>
-  )
+  );
 }
